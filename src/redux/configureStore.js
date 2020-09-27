309d32e25093765d0771b8959235e920a8fc0a4f
Tomasz Szydlowski <TSzydlowski@protonmail.com>
2020-09-27 21:17:22 +0200

    First thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import reduxImmetableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
 	const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
 
-	return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(reduxImmetableStateInvariant())));
+	return createStore(
+		rootReducer,
+		initialState,
+		composeEnhancers(applyMiddleware(thunk, reduxImmetableStateInvariant()))
+	);
 }
